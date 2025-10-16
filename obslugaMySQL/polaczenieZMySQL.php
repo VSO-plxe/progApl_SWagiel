791a1428dcd56b4e68693a395a13312072e8a8be
VSO_plxe <sebastianwagiel3@gmail.com>
2025-10-16 10:36:45 +0200

    aktualizacja pliku obslugi MySQL_dodanie nowych funkcji obslugujacych_20251016

--- obslugaMySQL/polaczenieZMySQL.php
+++ obslugaMySQL/polaczenieZMySQL.php
@@ -1,5 +1,5 @@
 <?php // podstawowe polaczenie z MySQL i tworzenie bazy danych
-    $servername = "localhost"; //lokalny serwer
+    $servername = "localhost"; // lokalny serwer
     $username = "root"; // Zmień na swoją nazwę użytkownika | domyślnie root
     $password = ""; // Zmień na swoje hasło | domyślnie puste
 
@@ -13,7 +13,7 @@
     echo "Connected successfully <br>";
 
     $database = "goetel"; // nazwa bazy danych
-    
+    /*
     // Tworzenie bazy danych
     $sql = "CREATE DATABASE $database";
     if (mysqli_query($conn, $sql)) {
@@ -21,14 +21,14 @@
     } else {
         echo "Error creating database: " . mysqli_error($conn . "<br>");
     }
-
+*/
     // Połączenie z konkretną bazą danych
     if (mysqli_select_db($conn, $database)) {
         echo "Connected to database successfully <br>";
     } else {
         echo "Error connecting to database: " . mysqli_error($conn) . "<br>";
     }
-
+/*
     // Tworzenie tabeli
     $sql = "CREATE TABLE MyGuests (
         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -36,35 +36,38 @@
         lastname VARCHAR(30) NOT NULL,
         email VARCHAR(50),
         reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
+    );";
     if (mysqli_query($conn, $sql)) {
         echo "Table MyGuests created successfully <br>";
     } else {
         echo "Error creating table: " . mysqli_error($conn) . "<br>";
     }
-
+*/
     // Wstawianie danych do tabeli
     $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-            VALUES ('John', 'Doe', 'john@example.com')";
+            VALUES ('John', 'Doe', 'john@example.com');";
     if (mysqli_query($conn, $sql)) {
         echo "New record created successfully <br>";
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn) . "<br>";
     }
-
+/*  Problematyczne od PHP v8 - zamiast mysqli_multi_query używać mysqli_query
     // Wstawianie wielu rekordów
     $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES 
-            ('Mary', 'Moe', 'mary@example.com')";
+            ('Mary', 'Moe', 'mary@example.com');";
     $sql .= "INSERT INTO MyGuests (firstname, lastname, email) VALUES 
-            ('Julie', 'Dooley', 'julie@example.com')";
+            ('Julie', 'Dooley', 'julie@example.com');";
     if (mysqli_multi_query($conn, $sql)) {
         echo "New records created successfully <br>";
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn) . "<br>";
     }
 
-    // Pobieranie danych z tabeli
-    $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    // Wyczyść pozostałe wyniki z multi_query (od PHP v8 - polecenie potrzebne do wykonania mysli_multi_query)
+    while (mysqli_more_results($conn) && mysqli_next_result($conn));
+*/
+    // Pobieranie danych z tabeli           
+    $sql = "SELECT id, firstname, lastname FROM MyGuests;";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
@@ -72,6 +75,40 @@
         while($row = mysqli_fetch_assoc($result)) {
             echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
         }
+    } else {
+        echo "0 results <br>";
+    }
+
+    $result = mysqli_query($conn, $sql);    // ponowne wykonanie zapytania poprzednie zostało "zużyte"
+    if (mysqli_num_rows($result) > 0) {
+        // sposób 2 - wyświetlanie danych z każdej kolumny
+        while($row = mysqli_fetch_row($result)) {
+            echo "id: " . $row[0]. " - Name: " . $row[1]. " " . $row[2]. "<br>";
+        }
+    } else {
+        echo "0 results <br>";
+    }
+
+    // Generowanie danych jako tabela HTML
+    $result = mysqli_query($conn, $sql);    // ponowne wykonanie zapytania poprzednie zostało "zużyte"
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table>";
+        while($row = mysqli_fetch_row($result)) {
+            echo "<tr><td>" . $row[0]. "</td><td>" . $row[1]. " " . $row[2]. "</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results <br>";
+    }
+
+    // Generowanie listy HTML
+    $result = mysqli_query($conn, $sql);    // ponowne wykonanie zapytania poprzednie zostało "zużyte"
+    if (mysqli_num_rows($result) > 0) {
+        echo "<ul>";
+        while($row = mysqli_fetch_row($result)) {
+            echo "<li>" . $row[0]. " - " . $row[1]. " " . $row[2]. "</li>";
+        }
+        echo "</ul>";
     } else {
         echo "0 results <br>";
     }
